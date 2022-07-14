275115275f3e9fd36e1595141fd13f9f1edb2739
PK <pradeepdhital124@gmail.com>
2022-07-14 19:20:59 +1000

    css appliee

--- src/Components/CountryDetails.js
+++ src/Components/CountryDetails.js
@@ -4,19 +4,21 @@ import ListGroup from "react-bootstrap/ListGroup";
 export const CountryDetails = ({ countryDetails, country }) => {
   return (
     <div>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "18rem", backgroundColor:"" }}>
         <Card.Img variant="top" src={countryDetails?.flags?.png} />
         <Card.Body>
-          <Card.Title> {country} </Card.Title>
+          <Card.Title> {countryDetails?.name?.common} </Card.Title>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>
+            <ListGroup.Item className="capital_city">
               Capital City: {countryDetails.capital}
             </ListGroup.Item>
-            <ListGroup.Item>
+            <ListGroup.Item className="population">
               population: {countryDetails.population}
             </ListGroup.Item>
-            <ListGroup.Item>Area: {countryDetails.area}</ListGroup.Item>
-            <ListGroup.Item>
+            <ListGroup.Item className="area">
+              Area: {countryDetails.area}
+            </ListGroup.Item>
+            <ListGroup.Item className="language">
               languages:
               {countryDetails?.languages &&
                 countryDetails?.languages[
@@ -24,16 +26,6 @@ export const CountryDetails = ({ countryDetails, country }) => {
                 ]}
             </ListGroup.Item>
           </ListGroup>
-
-          {/* <Card.Text>
-            It falls in {countryDetails?.continents[0]} continent. Its currency
-            is called
-            {
-              countryDetails?.currencies[
-                Object.keys(countryDetails.currencies)[0]
-              ][0]
-            }
-          </Card.Text> */}
         </Card.Body>
       </Card>
     </div>
